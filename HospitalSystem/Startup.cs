b2aeae0cc78ed4851b08fe02698deed3e859e0ea
Onur Kınay <ben@onurkinay.com.tr>
2023-12-18 20:11:28 +0300

    added user system

---
+++ HospitalSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HospitalSystem.Startup))]
+namespace HospitalSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
