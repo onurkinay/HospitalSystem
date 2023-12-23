b100782763d9f1d3952a7682ea233f0af4e7bb63
Onur Kınay <ben@onurkinay.com.tr>
2023-12-23 14:57:35 +0300

    static to const

---
+++ HospitalSystem/Models/MyConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalSystem.Models
+{
+    public static class MyConstants
+    {
+        public const string RoleAdmin = "Admin";
+        public const string RolePatient = "Patient";
+        public const string RoleDoctor = "Doctor";
+        public const string RoleAccountant = "Accountant";
+    }
+}
